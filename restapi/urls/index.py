0310edf58614c3f044b627b1ec509b625d4fe2be
takohack <1349607493@qq.com>
2022-05-01 15:41:58 +0800

    add redis

--- restapi/urls/index.py
+++ restapi/urls/index.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from restapi.views.index import index
+from restapi.views.index import get_comments
+from restapi.views.index import update_comments
 
 urlpatterns = [
     path("", index, name="index"),
     path("courses/", include("restapi.urls.course.index")),
-    path("user/",include("restapi.urls.user.index"))
+    path("user/",include("restapi.urls.user.index")),
+    path("getcomments/",get_comments,name="get_comments"),
+    path("updatecomments",update_comments,name="update_comments")
 ]
--- restapi/views/index.py
+++ restapi/views/index.py
@@ -1,6 +1,37 @@
 
+from curses.ascii import NUL
+from tokenize import Comment
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.cache import cache
+from django.http import JsonResponse
 
 def index(request):
     return render(request,"index.html")
+
+
+def get_comments(request):
+    print(cache.keys('*'))
+    # cache.set("comments", "wuhu",timeout=None)
+    if cache.has_key("comments"):
+        comments = cache.get("comments")
+        return JsonResponse({
+        'result':comments
+    })
+    else:
+        return JsonResponse({
+        'result':"fali"
+    })
+
+def update_comments(request):
+    data = request.GET
+    if not data:
+        return JsonResponse({
+        'result':"fail"
+    })
+    else:
+        comments = data.get('comments')
+        cache.set("comments",comments,timeout=None)
+        return JsonResponse({
+        'result':comments
+    })
